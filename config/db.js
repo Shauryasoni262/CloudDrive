92ab0e3d5fc04abcfccd66ddbb50d8b3294eaa7c
Shauryasoni262 <shauryasoni262@gmail.com>
2025-07-18 03:23:37 +0530

    Changes Mongodb url

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ function connectToDB() {
     console.log("✅ MongoDB connected successfully");
   })
   .catch((err) => {
-    console.error("❌ MongoDB connection error:", err.message);
+    console.error("❌ MongoDB connection errors:", err.message);
   });
 }
 
